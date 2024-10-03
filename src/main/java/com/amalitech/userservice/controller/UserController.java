864698183fef4444bca57c4350e8420c4dd4fcea
nccharles <nccharles1@gmail.com>
2024-10-03 15:17:55 +0200

    Finalized services setup

---
+++ src/main/java/com/amalitech/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.amalitech.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "This the user ID from Params: " + userId;
+    }
+}
+
